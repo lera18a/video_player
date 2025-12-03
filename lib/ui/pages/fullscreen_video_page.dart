063b1e0c74b466fd0789312db019ae408bd49fb7
Badrakova Valeria <lera_18a@mail.ru>
2025-12-03 18:41:15 +0300

    update fullscreen

--- lib/ui/pages/fullscreen_video_page.dart
+++ lib/ui/pages/fullscreen_video_page.dart
@@ -1,13 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:test_video_player/bloc/data_bloc/data_bloc.dart';
 import 'package:test_video_player/ui/ui_models/entities_icon.dart';
 import 'package:test_video_player/ui/ui_models/video_models/video_nav.dart';
 import 'package:test_video_player/ui/ui_models/video_models/video_play.dart';
 import 'package:test_video_player/ui/ui_models/video_models/video_slider.dart';
 import 'package:test_video_player/video_player_icons_icons.dart';
+import 'package:video_player/video_player.dart';
 
 class FullScreenVideoPage extends StatelessWidget {
-  const FullScreenVideoPage({super.key});
+  final VideoPlayerController? controller;
+
+  const FullScreenVideoPage({super.key, required this.controller});
 
   Future<void> _goPortraitScreen() async {
     await SystemChrome.setPreferredOrientations([
@@ -19,28 +24,35 @@ class FullScreenVideoPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      alignment: .center,
-      children: [
-        const Positioned.fill(bottom: -26, child: VideoPlay()),
-        Center(child: const VideoNav()),
-        Positioned(
-          bottom: 34.5,
-          left: 16,
-          right: 24,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Expanded(child: const VideoSlider()),
-              const SizedBox(width: 24),
-              EntitiesIcon(
-                onPressed: _goPortraitScreen,
-                icon: VideoPlayerIcons.diagonal,
+    return BlocBuilder<DataBloc, DataState>(
+      builder: (context, state) {
+        return Stack(
+          alignment: .center,
+          children: [
+            Positioned.fill(
+              bottom: -26,
+              child: VideoPlay(controller: controller),
+            ),
+            Center(child: const VideoNav()),
+            Positioned(
+              bottom: 34.5,
+              left: 16,
+              right: 24,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Expanded(child: VideoSlider(controller: controller)),
+                  const SizedBox(width: 24),
+                  EntitiesIcon(
+                    onPressed: _goPortraitScreen,
+                    icon: VideoPlayerIcons.diagonal,
+                  ),
+                ],
               ),
-            ],
-          ),
-        ),
-      ],
+            ),
+          ],
+        );
+      },
     );
   }
 }
