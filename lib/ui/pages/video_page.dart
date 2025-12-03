45113256a0e55ade1656b7f82187282c73c2c3a1
Badrakova Valeria <lera_18a@mail.ru>
2025-12-03 18:41:44 +0300

    update video page

--- lib/ui/pages/video_page.dart
+++ lib/ui/pages/video_page.dart
@@ -1,16 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:test_video_player/bloc/volume_bloc/volume_bloc.dart';
+import 'package:test_video_player/ui/logic_ui/app_volume.dart';
 import 'package:test_video_player/ui/ui_models/entities_icon.dart';
 import 'package:test_video_player/ui/ui_models/video_models/video_nav.dart';
 import 'package:test_video_player/ui/ui_models/video_models/video_play.dart';
 import 'package:test_video_player/ui/ui_models/video_models/video_slider.dart';
 import 'package:test_video_player/video_player_icons_icons.dart';
+import 'package:video_player/video_player.dart';
 
 class VideoPage extends StatelessWidget {
-  const VideoPage({super.key, required this.isFullScreen});
   final bool isFullScreen;
+  final VideoPlayerController? controller;
+
+  const VideoPage({super.key, required this.isFullScreen, this.controller});
+
   Future<void> _goFullScreen() async {
     await SystemChrome.setPreferredOrientations([
       DeviceOrientation.landscapeLeft,
@@ -20,44 +23,35 @@ class VideoPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<VolumeBloc, VolumeState>(
-      builder: (context, state) {
-        return Column(
+    return Column(
+      children: [
+        Stack(
+          alignment: .center,
           children: [
-            Stack(alignment: .center, children: [VideoPlay(), VideoNav()]),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 34.5),
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                child: SizedBox(
-                  height: 44,
-                  child: Row(
-                    children: [
-                      Expanded(child: VideoSlider()),
-                      SizedBox(width: 12),
-                      EntitiesIcon(
-                        onPressed: () {
-                          context.read<VolumeBloc>().add(VolumeEvent());
-                        },
-                        icon: state.isMuted
-                            ? Icons.volume_off_rounded
-                            : VideoPlayerIcons.volume,
-                        size: 14.77,
-                      ),
-                      SizedBox(width: 12),
-                      if (!isFullScreen)
-                        EntitiesIcon(
-                          onPressed: _goFullScreen,
-                          icon: VideoPlayerIcons.bigscreen,
-                        ),
-                    ],
-                  ),
-                ),
-              ),
-            ),
+            VideoPlay(controller: controller!),
+            VideoNav(),
           ],
-        );
-      },
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 34.5, horizontal: 16.0),
+          child: SizedBox(
+            height: 44,
+            child: Row(
+              children: [
+                Expanded(child: VideoSlider(controller: controller)),
+                SizedBox(width: 12),
+                AppVolume(),
+                SizedBox(width: 12),
+                if (!isFullScreen)
+                  EntitiesIcon(
+                    onPressed: _goFullScreen,
+                    icon: VideoPlayerIcons.bigscreen,
+                  ),
+              ],
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
